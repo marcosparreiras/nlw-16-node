89b02dd308596429c6fbce904722164a6570e4af
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 17:01:22 -0300

    feat: start a http server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/test", async (request, reply) => {
+  return reply.status(200).send({ message: "Hello World" });
+});
+
+app.listen({ port: Number(process.env.PORT) }).then(() => {
+  console.log(`Server is running on port ${process.env.PORT} 🚀`);
+});
