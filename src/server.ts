a72c5bfdbb8f2aa68c5e8b353b2231305d543eca
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 17:25:45 -0300

    chore: setup prisma orm

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get("/test", async (request, reply) => {
-  return reply.status(200).send({ message: "Hello World" });
+app.get("/cadastrar", async (request, reply) => {
+  await prisma.trip.create({
+    data: {
+      destination: "Florianopolis",
+      endsAt: new Date(),
+      startsAt: new Date(),
+    },
+  });
+  return reply.status(200).send({ message: "Registro cadastrado com sucesso" });
+});
+
+app.get("/listar", async (request, reply) => {
+  const trips = await prisma.trip.findMany();
+  return reply.status(200).send({ trips });
 });
 
 app.listen({ port: Number(process.env.PORT) }).then(() => {
